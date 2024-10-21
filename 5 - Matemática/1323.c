008048338655b17514bef88e27b03bb47bca807c
Matheus Oliveira da Silva <oliveiradsmatheus@gmail.com>
2024-10-21 17:06:17 -0300

    1323 - Feynman

---
+++ 5 - Matemática/1323.c
@@ -0,0 +1,21 @@
+    #include <stdio.h>
+
+    int main(void) {
+        int ladoMaior, areaMaior, ladoMenor, areaMenor, qtde;
+
+        scanf("%d", &ladoMaior);
+        while (ladoMaior != 0) {
+            areaMaior = ladoMaior*ladoMaior;
+            qtde = 0;
+            ladoMenor = 1;
+            areaMenor = ladoMenor*ladoMenor;
+            while(areaMenor <= areaMaior) {
+                qtde += areaMenor;
+                ladoMenor ++;
+                areaMenor = ladoMenor*ladoMenor;
+            }
+            printf("%d\n",qtde);
+            scanf("%d", &ladoMaior);
+        }
+        return 0;
+    }
