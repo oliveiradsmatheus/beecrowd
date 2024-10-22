ee4fc01b7f301798f1298ed54b3d3dce94616809
Matheus Oliveira da Silva <oliveiradsmatheus@gmail.com>
2024-10-22 17:30:24 -0300

    1436 - Jogo do Tijolo

---
+++ 5 - Matemática/1436.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int casos, caso=0, qtde, equipe[10], i;
+
+    scanf("%d", &casos);
+    while (caso < casos) {
+        scanf("%d", &qtde);
+        for (i = 0; i < qtde; i++)
+            scanf("%d", &equipe[i]);
+        printf("Case %d: %d\n",caso+1, equipe[qtde/2]);
+        caso++;
+    }
+    return 0;
+}
