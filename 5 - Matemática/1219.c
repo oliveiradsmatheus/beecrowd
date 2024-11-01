0afeabfeb413d63cb54e998653d060287b2e5851
Matheus Oliveira da Silva <oliveiradsmatheus@gmail.com>
2024-11-01 17:26:17 -0300

    1219 - Flores Coloridas

--- 5 - Matemática/1219.c
+++ 5 - Matemática/1219.c
@@ -7,13 +7,13 @@ int main(void) {
     int a, b, c;
     double P, AreaT, Rmenor, Rmaior, Girassois, Violetas, Rosas;
     while (scanf("%d %d %d", &a, &b, &c) != EOF) {
-        P = (double)(a + b + c) / 2;
-        AreaT = sqrt(P*(P-a)*(P-b)*(P-c));
-        Rmenor = 2*AreaT/(a+b+c);
+        P = (double) (a + b + c) / 2;
+        AreaT = sqrt(P * (P - a) * (P - b) * (P - c));
+        Rmenor = 2 * AreaT / (a + b + c);
         Rosas = PI * Rmenor * Rmenor;
         Violetas = AreaT - Rosas;
-        Rmaior = ((a*b*c)/AreaT)/4;
+        Rmaior = ((a * b * c) / AreaT) / 4;
         Girassois = (PI * Rmaior * Rmaior) - AreaT;
-        printf("%.4lf %.4lf %.4lf\n", Girassois ,Violetas, Rosas);
+        printf("%.4lf %.4lf %.4lf\n", Girassois, Violetas, Rosas);
     }
 }
